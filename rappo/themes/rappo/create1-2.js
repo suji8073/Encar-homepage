7080d16e6533bc44dc4e1d7c7a7401f612b5d160
suji8073 <suzy8073@naver.com>
2022-03-25 20:47:33 +0900

    난이도 javascript 추가

---
+++ rappo/themes/rappo/create1-2.js
@@ -0,0 +1,25 @@
+function dep1() {
+  const btnElement = document.getElementById("level");
+  btnElement.innerText = "1";
+  btnElement.style.color = "#000";
+}
+function dep2() {
+  const btnElement = document.getElementById("level");
+  btnElement.innerText = "2";
+  btnElement.style.color = "#000";
+}
+function dep3() {
+  const btnElement = document.getElementById("level");
+  btnElement.innerText = "3";
+  btnElement.style.color = "#000";
+}
+function dep4() {
+  const btnElement = document.getElementById("level");
+  btnElement.innerText = "4";
+  btnElement.style.color = "#000";
+}
+function dep5() {
+    const btnElement = document.getElementById("level");
+    btnElement.innerText = "5";
+    btnElement.style.color = "#000";
+  }
